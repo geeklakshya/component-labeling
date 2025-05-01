697492d3e165b8d74998383e0cf85ea995e242c5
geekylakshya <lakahayk75785@gmail.com>
2025-05-01 16:18:30 +0530

    Update component_labeling.py

--- image-api/component_labeling.py
+++ image-api/component_labeling.py
@@ -383,4 +383,7 @@ if __name__ == "__main__":
     print("🔄 CORS is enabled for all origins")
     print("📂 Upload directory:", os.path.abspath(UPLOAD_DIR))
     print("📂 Output directory:", os.path.abspath(OUTPUT_DIR))
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
